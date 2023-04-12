5f85f4df902a77225f31da09922055ec3f352658
bobello.gabriel@gmail.com <GBobello>
2023-04-12 02:13:16 -0300

    First Commit

---
+++ src/views/gdMain.dfm
@@ -0,0 +1,17 @@
+object frMain: TfrMain
+  Left = 0
+  Top = 0
+  ClientHeight = 471
+  ClientWidth = 726
+  Color = clBtnFace
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -11
+  Font.Name = 'Tahoma'
+  Font.Style = []
+  OldCreateOrder = False
+  OnActivate = FormActivate
+  OnKeyDown = FormKeyDown
+  PixelsPerInch = 96
+  TextHeight = 13
+end
---
+++ src/views/telas/gdLogin.pas
@@ -0,0 +1,24 @@
+unit gdLogin;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs;
+
+type
+  TfrLogin = class(TForm)
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  frLogin: TfrLogin;
+
+implementation
+
+{$R *.dfm}
+
+end.
